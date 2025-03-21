ac7c4852bf2c7a133e47cd62403b3353d6312ec5
Vijay <vijaynatarajan@protonmail.com>
2025-03-21 16:15:35 -0400

    added user model

---
+++ src/models/user.ts
@@ -0,0 +1,50 @@
+import { Schema, model, Document, ObjectId } from "mongoose";
+
+interface IUser extends Document {
+    username: string;
+    email: string;
+    thoughts: ObjectId[];
+    friends: ObjectId[];
+}
+
+const userSchema = new Schema<IUser>(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thoughts',
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+)
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends?.length
+});
+
+const User = model('user', userSchema);
+
+export default User;
